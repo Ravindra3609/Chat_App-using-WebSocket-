7c584aae96247935262af7cc1abb55973e93d357
Ravindra <84234685+Ravindra3609@users.noreply.github.com>
2023-04-05 10:59:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  socket.on("user-message", (message) => {
+    // console.log("A New user message " , message);
+    io.emit("message" , message);
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public.index.html");
+});
+
+server.listen(9000, () => console.log(`server started at PORT : 9000`));
